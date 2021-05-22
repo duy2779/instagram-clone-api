56772cdeeafa927b3d7c84cb5c5d5be8c7306b86
duy <jultqd99@gmail.com>
2021-05-22 13:48:30 +0700

    fix get_posts api - get_posts by pagination instead

--- post/views.py
+++ post/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Post, PostComment
 from .serializers import PostSerializer, PostCommentSerializer
@@ -18,5 +19,8 @@ def get_posts(request):
     users_following = user.following.all()
     posts = Post.objects.filter(Q(user=user) | Q(
         user__in=users_following)).order_by('-created')
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
+    paginator = PageNumberPagination()
+    paginator.page_size = 5
+    result_page = paginator.paginate_queryset(posts, request)
+    serializer = PostSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)

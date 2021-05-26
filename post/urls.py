4b83c79233d6a85df63f9b2f07658cbbc0914d44
duy <jultqd99@gmail.com>
2021-05-26 19:14:38 +0700

    create api - create create_post api

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import get_posts, toggle_like, add_comment
+from .views import get_posts, toggle_like, add_comment, create_post
 
 urlpatterns = [
     path('posts', get_posts, name='posts'),
     path('toggle-like/<str:post_id>', toggle_like, name='toggle-like'),
     path('add-comment/<str:post_id>', add_comment, name='add-comment'),
+    path('create-post', create_post, name='create-post'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.db.models import Q
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .models import Post, PostComment
 from .serializers import PostSerializer, PostCommentSerializer
@@ -56,3 +57,17 @@ def add_comment(request, post_id):
     except Exception as e:
         message = {'detail': e}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+@parser_classes([MultiPartParser, FormParser])
+def create_post(request):
+    print(request.data)
+    user_id = request.data.get('user') or None
+    caption = request.data.get('caption') or None
+    image = request.data.get('image') or None
+    post = Post.objects.create(
+        user=request.user, caption=caption, image=image)
+    serializer = PostSerializer(post, many=False)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
